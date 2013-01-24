33e3facd3283149ae412d9be973bb090084a4768
LimJihyun <ljhzzang91@naver.com>
2013-01-24 17:54:43 +0900

    test1 branch

--- jihyun_test/src/jihyun_test.java
+++ jihyun_test/src/jihyun_test.java
@@ -4,5 +4,10 @@ public class jihyun_test
 	public static void main(String[] args) 
 	{
 		System.out.println("jihyun_test");
+		System.out.println("jihyun_test");
+
+		System.out.println("jihyun_test");
+		System.out.println("jihyun_test");
+		
 	}
 }
